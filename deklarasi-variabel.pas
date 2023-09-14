74ff628da1ab265312256f1e5e469da8b3415ab3
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 20:40:06 +0700

    Create deklarasi-variabel.pas

---
+++ deklarasi-variabel.pas
@@ -0,0 +1,9 @@
+program ContohDeklarasiVariabel;
+var
+  // Deklarasi variabel-variabel di sini
+  nilai1, nilai2: Integer;
+  nama: string;
+  nilaiFloat: Real;
+begin
+  // Kode program utama disini
+end.
