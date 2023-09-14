eb2e59919f82fe15add35f238a248cd268b97d50
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 21:05:53 +0700

    Create lebih dari sama dengan.pas

---
+++ operator-perbandingan/lebih dari sama dengan.pas
@@ -0,0 +1,14 @@
+program OperatorLebihDariSamaDengan;
+var
+  a, b: integer;
+begin
+  a := 7;
+  b := 5;
+  
+  if (a >= b) then
+    writeln('a lebih dari atau sama dengan b')
+  else
+    writeln('a tidak lebih dari atau sama dengan b');
+end.
+
+\
