402fc464abccadaef2f01620c9f36779515d9f00
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 20:43:35 +0700

    Create deklarasi-konstanta.pas

---
+++ deklarasi-konstanta.pas
@@ -0,0 +1,8 @@
+program ContohDeklarasiKonstanta;
+const
+  PI: Real = 3.14159265;
+  NAMA_PROGRAM: string = 'Program Contoh';
+  USIA_PENGGUNA: Integer = 30;
+begin
+  // Kode program utama disini
+end.
