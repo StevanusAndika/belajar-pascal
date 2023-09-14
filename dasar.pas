e95a42985cd6aacac7c736fb7eae0141369e7a23
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 20:39:01 +0700

    Create dasar.pas

---
+++ dasar.pas
@@ -0,0 +1,8 @@
+program Hello;
+begin
+//write berfungsi untuk menampilkan output pada baris yang yang sama
+writeln ('Hello World');
+//writeln untuk menampilkan output pada baris yang baru
+writeln('Baris baru');
+end.
+
