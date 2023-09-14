25789fa91848af92ce95be4bb765357f63ab296f
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 21:00:46 +0700

    Create tidak-sama dengan.pas

---
+++ operator-perbandingan/tidak-sama dengan.pas
@@ -0,0 +1,12 @@
+program OperatorTidakSamaDengan;
+var
+  a, b: integer;
+begin
+  a := 5;
+  b := 7;
+  
+  if (a <> b) then
+    writeln('a tidak sama dengan b')
+  else
+    writeln('a sama dengan b');
+end.
