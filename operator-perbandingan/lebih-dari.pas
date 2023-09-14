e818cb6453f43e43addd83eaace235d1ab2f6234
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 21:02:03 +0700

    Create lebih-dari.pas

---
+++ operator-perbandingan/lebih-dari.pas
@@ -0,0 +1,12 @@
+program OperatorLebihDari;
+var
+  a, b: integer;
+begin
+  a := 5;
+  b := 7;
+  
+  if (a > b) then
+    writeln('a lebih dari b')
+  else
+    writeln('a tidak lebih dari b');
+end.
