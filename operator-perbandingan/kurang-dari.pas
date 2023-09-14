41f98f9ccaf24bcc75c2d8ce9ca9afca821296a6
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 21:01:17 +0700

    Create kurang-dari.pas

---
+++ operator-perbandingan/kurang-dari.pas
@@ -0,0 +1,12 @@
+program OperatorKurangDari;
+var
+  a, b: integer;
+begin
+  a := 5;
+  b := 7;
+  
+  if (a < b) then
+    writeln('a kurang dari b')
+  else
+    writeln('a tidak kurang dari b');
+end.
