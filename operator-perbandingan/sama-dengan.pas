b62eb116cecbededd39c30f031878126e05ce8a9
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 21:00:01 +0700

    Create sama-dengan.pas

---
+++ operator-perbandingan/sama-dengan.pas
@@ -0,0 +1,12 @@
+program OperatorSamaDengan;
+var
+  a, b: integer;
+begin
+  a := 5;
+  b := 5;
+  
+  if (a = b) then
+    writeln('a sama dengan b')
+  else
+    writeln('a tidak sama dengan b');
+end.
