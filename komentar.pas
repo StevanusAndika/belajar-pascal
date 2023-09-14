eeb9a3bb178fe066363363cc0f08e17ca5891238
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 20:50:10 +0700

    Create komentar.pas

---
+++ komentar.pas
@@ -0,0 +1,19 @@
+program KomentarSatuBaris;
+
+begin
+//ini komentar satu baris
+  writeln('Ini adalah contoh komentar satu baris.');
+
+  
+ {
+    Ini adalah komentar
+    yang terdiri dari
+    beberapa baris.
+  }
+  writeln('Ini adalah contoh komentar banyak baris.');
+
+ 
+
+  
+
+end.
