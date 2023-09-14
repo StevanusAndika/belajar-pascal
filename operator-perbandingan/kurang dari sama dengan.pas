511debb100558b86c539aa28aacfdc9a4e7f0dcd
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-14 21:05:15 +0700

    Create kurang dari sama dengan.pas

---
+++ operator-perbandingan/kurang dari sama dengan.pas
@@ -0,0 +1,12 @@
+program OperatorKurangDariSamaDengan;
+var
+  a, b: integer;
+begin
+  a := 5;
+  b := 5;
+  
+  if (a <= b) then
+    writeln('a kurang dari atau sama dengan b')
+  else
+    writeln('a tidak kurang dari atau sama dengan b');
+end.
